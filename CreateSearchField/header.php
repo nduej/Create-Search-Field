66282cf40ffc7d0f61317644d0e6906c47328074
Michael Ndue <95193983+nduej@users.noreply.github.com>
2023-03-22 15:38:46 +0100

    Add files via upload

---
+++ CreateSearchField/header.php
@@ -0,0 +1,14 @@
+<?php
+include'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create Search Field </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
